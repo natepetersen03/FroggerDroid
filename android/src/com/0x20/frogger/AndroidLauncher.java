4c3c6c590e2d814341ad88ccb02208510bb0a088
dnelson78 <dnelson78@gatech.edu>
2023-02-26 11:51:36 -0500

    build: LibGDX project generation

---
+++ android/src/com/0x20/frogger/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.0x20.frogger;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.0x20.frogger.FroggerDroid;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FroggerDroid(), config);
+	}
+}
