b9655518831571437db1a2ec35f1ec8ab9d79789
dnelson78 <dnelson78@gatech.edu>
2023-03-14 00:24:54 -0400

    Separated TileStruct from TileDatabase, which is now a singleton

--- core/src/com/x20/frogger/data/TileDatabase.java
+++ core/src/com/x20/frogger/data/TileDatabase.java
@@ -1,5 +1,6 @@
 package com.x20.frogger.data;
 
+import com.x20.frogger.game.GameLogic;
 import com.x20.frogger.game.Tile;
 import com.x20.frogger.graphics.TileRenderData;
 
@@ -8,6 +9,7 @@ import java.util.Map;
 
 public class TileDatabase {
 
+    private static TileDatabase instance;
     private static Map<String, TileStruct> database;
     {
         database = new HashMap<String, TileStruct>();
@@ -16,15 +18,31 @@ public class TileDatabase {
                 new Tile("road", false),
                 new TileRenderData(0, 0)
             ));
+        database.put("water",
+            new TileStruct(
+                new Tile("water", false, true, 0.1f),
+                new TileRenderData(0, 0)
+            ));
+        database.put("safe",
+            new TileStruct(
+                new Tile("safe", false, false, 0.1f),
+                new TileRenderData(0, 0)
+            ));
+        database.put("safe",
+            new TileStruct(
+                new Tile("safe", false, false, 0.1f),
+                new TileRenderData(0, 0)
+            ));
     }
 
-    public class TileStruct {
-        private Tile tile;
-        private TileRenderData renderData;
+    private TileDatabase() {
+        System.out.println("Tile database initialized");
+    }
 
-        public TileStruct(Tile tile, TileRenderData renderData) {
-            this.tile = tile;
-            this.renderData = renderData;
+    public static synchronized TileDatabase getInstance() {
+        if (instance == null) {
+            instance = new TileDatabase();
         }
+        return instance;
     }
 }
---
+++ core/src/com/x20/frogger/data/TileStruct.java
@@ -0,0 +1,14 @@
+package com.x20.frogger.data;
+
+import com.x20.frogger.game.Tile;
+import com.x20.frogger.graphics.TileRenderData;
+
+public class TileStruct {
+    private Tile tile;
+    private TileRenderData renderData;
+
+    public TileStruct(Tile tile, TileRenderData renderData) {
+        this.tile = tile;
+        this.renderData = renderData;
+    }
+}
