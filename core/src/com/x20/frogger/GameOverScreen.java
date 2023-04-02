ddb9a61faa7b220d5b2a7a0d630f4b9d6a5d3d17
dpark357 <dpark357@gatech.edu>
2023-04-02 18:45:12 -0400

    game over screen fully functional

--- core/src/com/x20/frogger/GameOverScreen.java
+++ core/src/com/x20/frogger/GameOverScreen.java
@@ -17,6 +17,7 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.x20.frogger.GUI.GameConfigViewModel;
+import com.x20.frogger.game.GameLogic;
 import com.x20.frogger.graphics.AssetManagerSingleton;
 
 public class GameOverScreen extends ScreenAdapter {
@@ -25,6 +26,10 @@ public class GameOverScreen extends ScreenAdapter {
     private Viewport viewport;
     private Stage stage;
     private Skin skin;
+
+    private Label scoreLabel;
+    private TextButton restartButton;
+    private TextButton exitButton;
 
     public GameOverScreen(final FroggerDroid game) {
         this.game = game;
@@ -45,21 +50,16 @@ public class GameOverScreen extends ScreenAdapter {
     @Override
     public void show() {
         // load all assets before displaying ready
-        //game.getAssetManager().finishLoading();
         AssetManagerSingleton.getInstance().getAssetManager().finishLoading();
     }
 
     @Override
     public void render(float delta) {
         // Clear the screen with a dark blue background
-        // ScreenUtils.clear(0, 0, 0.2f, 1);
         Gdx.gl.glClearColor(0, 0, 0.2f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        // update stage and all its actors
         stage.act();
-
-        // draw stage and all its actors
         stage.draw();
 
     }
@@ -78,35 +78,74 @@ public class GameOverScreen extends ScreenAdapter {
     private void constructGUI() {
         stage = new Stage(new ExtendViewport(500, 480));
         Gdx.input.setInputProcessor(stage);
-
         Table table = new Table();
         table.setFillParent(true);
 
-        Label label = new Label("Game Over", skin, "dark-bg");
-        table.add(label).colspan(2);
+        setHeaderLabel(table);
+        setScoreLabel(table);
+        setRestartButton(table);
+        setExitButton(table);
 
-        table.row();
-        table.add().colspan(2);
-
-        table.row();
-        label = new Label(null, skin);
-        table.add(label);
-
-        label = new Label(null, skin);
-        table.add(label);
-
-        table.row();
-        label = new Label("Your Score: ", skin);
-        table.add(label).padTop(5.0f).padBottom(5.0f).colspan(2);
-
-        table.row();
-        TextButton textButton = new TextButton("Restart", skin, "centeredLabel");
-        table.add(textButton).pad(5.0f);
-
-        textButton = new TextButton("Exit", skin, "centeredLabel");
-        table.add(textButton).pad(5.0f);
         stage.addActor(table);
+    }
 
+    private void setHeaderLabel(Table table) {
+        Label label = new Label("Game Over!", skin, "dark-bg");
+        table.add(label)
+                .spaceBottom(8.0f)
+                .fillX()
+                .minWidth(300.0f)
+                .minHeight(42.0f)
+                .colspan(3);
+        label.setAlignment(Align.center);
+        table.row();
+    }
+
+    private void setScoreLabel(Table table) {
+        int score = GameLogic.getInstance().getScore();
+        scoreLabel = new Label(" Your Score: " + "[#00FF00]" + score + " ", skin);
+        table.add(scoreLabel)
+                .spaceTop(8.0f)
+                .spaceBottom(32.0f)
+                .fillX()
+                .minHeight(42.0f)
+                .colspan(3);
+        scoreLabel.setAlignment(Align.center);
+        table.row();
+    }
+
+    private void setRestartButton(Table table) {
+        restartButton = new TextButton(" Restart ", skin, "centeredLabel");
+        table.add(restartButton)
+                .spaceBottom(8.0f)
+                .fillX()
+                .minWidth(300.0f)
+                .minHeight(42.0f)
+                .colspan(3);
+        restartButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                switchToGameConfigScreen();
+            }
+        });
+        table.row();
+    }
+
+    private void setExitButton(Table table) {
+        exitButton = new TextButton(" Exit ", skin, "centeredLabel");
+        table.add(exitButton)
+                .spaceBottom(8.0f)
+                .fillX()
+                .minWidth(300.0f)
+                .minHeight(42.0f)
+                .colspan(3);
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+                System.exit(-1);
+            }
+        });
     }
 
 }
