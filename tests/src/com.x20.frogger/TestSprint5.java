7dfde044a3c0fd0ab822a68a3523d17187c90d60
Nate Petersen <natepetersen@Nates-MacBook-Pro-4.local>
2023-04-17 00:17:57 -0400

    nate test

--- tests/src/com.x20.frogger/TestSprint5.java
+++ tests/src/com.x20.frogger/TestSprint5.java
@@ -228,4 +228,18 @@ public class TestSprint5 {
 
     }
 
+    @Test
+    public void testWinScreenAlive() {
+        gameLogic.setLives(5);
+        gameLogic.playerWin();
+        assertEquals(gameLogic.isDead(), false);
+    }
+    @Test
+    public void testLog8Log9OppositeDirections() {
+        LinkedList<Entity> testRow1 = gameLogic.getTileMap().getEntitiesAtRow(9);
+        LinkedList<Entity> testRow2 = gameLogic.getTileMap().getEntitiesAtRow(8);
+        float val = (((Log) (testRow1.get(2))).getVelocity().x) *
+                (((Log) (testRow2.get(2))).getVelocity().x);
+        assertEquals(val < 0, true);
+    }
 }
